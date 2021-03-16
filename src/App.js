8888282af516ad9aae0c9180aeb72d5c342dc94a
ChristinThoma <christin-thoma@t-online.de>
2021-03-16 21:39:38 +0100

    change footer responsive

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import {
   Link
 } from "react-router-dom";
 
-import { MDBFreeBird, MDBInput, MDBCol, MDBRow, MDBCardBody, MDBCardTitle, MDBBtn, MDBContainer, MDBEdgeHeader } from
-"mdbreact";
 
 import Footer from './components/Footer';
 import LandingPage from './components/LandingPage';
@@ -26,8 +24,9 @@ function App() {
    <Switch>
             <Route path="/arena" component={Arena}/>
             <Route path="/choosePlayer" component={Dashboard}/>
+            <Route path="/contact"component={LandingPage} />
             <Route exact path="/"component={LandingPage} />
-       
+     
     </Switch>
    <Footer />
 
@@ -40,79 +39,3 @@ function App() {
 export default App;
 
 
-
-{/* < >   
-<div class="container-fluid" >
-
-    <div style={{
-        backgroundColor: 'yellow',
-        backgroundImage:
-        `url(${process.env.PUBLIC_URL + '/img/colorful.jpg'})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-    }}>    
-  
-      <div class="container-fluid" >
-        <div class="row">
-            <div class="col-12">
-            <Header />
-            </div>
-        </div>
-        <div class="row">
-             <div class="col-4">
-            </div>
-            
-            <div class="col-4">
-            </div>
-        </div>
-  
-      </div>
-  
-     
-
-
-
-
-      <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3 ">
-            </div>
-            <div class="col-md-6 ">
-                <LandingPage />
-            </div>
-            <div class="col-md-3 ">
-            </div>
-        </div>
-      </div>
-    
-      <div class="container-fluid mt-5">
-        <div class="row">
-          <div class="col-md-8 bg-primary">
-            <p class="lead">lorem40 sdvnafvnaoefnvldfvsdfdsfvdfvdfvdsfvfdaibvufbsdfbdbbdbfffdbfbfdbsgdbgdfbdsgsdgdbdsbavuibubfvafbfbfbbwbwrbgvfbvibvjbvbsdvbsdbvsbjdvskdbvbvdjkbsajbvksabvsl</p>
-
-          </div>
-          <div class="col-md-4 bg-warning">
-            <p class="lead">Lorem 40 dsvibasbvsdvnösdnvöndsvnsdvnlödsnvksdnvönsajvnösadvknkasvnönfvnkafndsvnfvnsdvnkasnv</p>
-            
-          </div>
-          {/* <Switch>
-          <Route path="/landing">
-            <About />
-          </Route>
-          <Route path="/chooseCharacter">
-            <Users />
-          </Route>
-          <Route path="/playing">
-            <Home />
-          </Route>
-        </Switch> */}
-
-      //   </div>
-
-      //   <Footer />
-
-      // </div>
-      // </div> 
-      // </div>
-      // </>  */}
