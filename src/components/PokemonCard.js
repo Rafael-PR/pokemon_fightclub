e96ea42f3614a48e1e97273151ea12117fef036b
Rafael <76160982+Rafael-PR@users.noreply.github.com>
2021-03-20 16:02:31 +0100

    add pictures for pokemon

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -10,15 +10,59 @@ const PokemonCard = ({ pokemonData, onChoosePokemon}) => {
         history.push('/arena')
     }
 
+    console.log(pokemonData)
+
     return (
         <React.Fragment>
             <div className="col-md-3 col-sm-6 mb-5">
                 <div className="card">
                     <div className="card-header">
                         <h4>{name.toUpperCase()}</h4>
+                        
+                    </div>
+                    <div className="card-body">
                         <img src={pokemonData.sprites.front_default}/>
                     </div>
-                    <div className="card-body">Hier kommt dan ein Bild rein und evtl. noch die Superkraft</div>
+
+                    <div class="card-header">
+                        Abilities
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <div className="Card__types">
+                                {pokemonData.types.map(type => {
+                                return (<div className="Card__type">{type.type.name}</div>)})}
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <p className="title">Weight</p>
+                            <p>{pokemonData.weight}</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p className="title">Height</p>
+                            <p>{pokemonData.height}</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p className="title">Ability</p>
+                            <p>{pokemonData.abilities[0].ability.name}</p>
+                        </li>
+
+
+                    </ul>
+                    
+
+                    
+
+                    <div className="Card__info">
+                    
+                    
+                    
+            </div>
+
+
+
+
+
                     <button type="button" class="btn btn-primary" onClick={selectPokemon}>Choose Pokemon</button>
                 </div>
             </div>
